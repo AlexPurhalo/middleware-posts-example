a1601b7860bfde0c80deb48fbfc15d30845e8b3b
alexpurhalo <alexpurhalo@gmail.com>
2016-07-24 21:39:47 +0300

    acces-to-pages-watching

--- src/components/require_authorization.js
+++ src/components/require_authorization.js
@@ -3,9 +3,29 @@ import { connect } from 'react-redux';
 
 export default function(ComposedComponent) {
 	class Authentication extends Component {
+		// Access to using 'router' property
+		static contextTypes = {
+			router: React.PropTypes.object
+		};
+
+		// Redirect to root path if user are not authenticated
+		componentWillMount() {
+			if (!this.props.authenticated) {
+				this.context.router.push('/');
+			}
+		}
+
+		// Redirect to root page if user signed out
+		componentWillUpdate(nextProps) {
+			if (!nextProps.authenticated) {
+				this.context.router.push('/');
+			}
+		}
+
 		render() {
-			console.log(ComposedComponent);
-			console.log(this.props.authenticated);
+			console.log(this.context)
+			// console.log(ComposedComponent);
+			// console.log(this.props.authenticated);
 			return <ComposedComponent {...this.props} />
 		}
 	}
